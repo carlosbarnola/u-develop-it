e1b635e46812ec4214c48c0be8542260bc6f6188
Carlos Rojas <carlosrb206@gmail.com>
2021-11-28 14:25:06 -0500

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 //Router setup
 router.use(require('./candidatesRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./votersRoutes'));
 
 
 module.exports = router;
